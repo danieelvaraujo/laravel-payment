01db7ba6f82e978c5166a7583b4c05ff422ae25e
Daniel Valente <danieelvaraujo@hotmail.com>
2023-01-04 11:52:47 -0300

    feat: payment form layout created

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,53 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Make a payment</div>
 
                 <div class="card-body">
-                    {{ __('You are logged in!') }}
+                    <form
+                        action="#"
+                        method="POST"
+                        id="paymentForm"
+                    >
+                        @csrf
+                        <div class="row">
+                            <div class="col-auto">
+                                <label for="value">How much you want to pay?</label>
+                                <input
+                                    type="number"
+                                    min="5"
+                                    step="0.01"
+                                    class="form-control"
+                                    name="value"
+                                    value="{{ mt_rand(500, 100000) / 100 }}"
+                                    required
+                                >
+                            </div>
+                            <div class="col-auto">
+                                <label for="currency">Currency</label>
+                                <select
+                                    class="form-select"
+                                    name="currency"
+                                    required
+                                >
+                                    @foreach ($currencies as $currency)
+                                        <option value="{{ $currency->iso }}">
+                                            {{ strtoupper($currency->iso) }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
+                        <div class="text-center mt-3">
+                            <button
+                                class="btn btn-primary btn-lg"
+                                type="submit"
+                                id="payButton"
+                            >
+                                Pay
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
