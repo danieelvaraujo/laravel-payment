6f80962525ccd5a337f27e11fc969fdabf8809da
Daniel Valente <danieelvaraujo@hotmail.com>
2023-01-09 15:21:33 -0300

    chore: using paypal service to handle payment

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\PaypalService;
 use Illuminate\Http\Request;
 
 class PaymentController extends Controller
@@ -30,8 +31,9 @@ class PaymentController extends Controller
         ];
 
         $request->validate($rules);
-
-        return $request->all();
+        $paymentPlatform = resolve(PaypalService::class);
+        
+        return $paymentPlatform->handlePayment($request);
     }
 
     public function approval()
