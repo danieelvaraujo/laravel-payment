6dc8bf6eaa54bfe1da91883841602e8fd50bbec2
Daniel Valente <danieelvaraujo@hotmail.com>
2023-01-12 10:26:15 -0300

    chore: stripe 3d secure payent view

---
+++ resources/views/stripe/3d-secure.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Complete the security steps</div>
+
+                <div class="card-body">
+                    Now, follow the steps from your bank to complete this payment.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
