459acb4c09d012654ce0ddec9b56bb5cdebd802a
Daniel Valente <danieelvaraujo@hotmail.com>
2023-01-12 10:55:21 -0300

    chore: created plan model, migration and seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PaymentPlatformsTableSeeder::class,
-            CurrenciesTableSeeder::class
+            CurrenciesTableSeeder::class,
+            PlanSeeder::class
         ]);
     }
 }
